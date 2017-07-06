eb3dbcd07212613382edaf6dd57d5a88be3f6b57
shif.wang <shifwang@gmail.com>
2017-07-06 14:14:38 -0700

    changed several files in irf

--- skgarden/irf/__init__.py
+++
@@ -1,7 +0,0 @@
-from .ensemble import MondrianForestClassifier
-from .ensemble import MondrianForestRegressor
-from .tree import MondrianTreeClassifier
-from .tree import MondrianTreeRegressor
-
-__all__ = ["MondrianForestClassifier", "MondrianForestRegressor",
-           "MondrianTreeClassifier", "MondrianTreeRegressor"]
--- skgarden/irf/tree/setup.py
+++ skgarden/irf/tree/setup.py
@@ -36,4 +36,4 @@ def configuration(parent_package="", top_path=None):
 
 if __name__ == "__main__":
     from numpy.distutils.core import setup
-    setup(**configuration().todict())
+    setup(**configuration(top_path="").todict())
